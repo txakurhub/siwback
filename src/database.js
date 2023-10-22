6dec988698a9e3afa27a67028df93ad0ed28f715
unknown <me.leandrop@gmail.com>
2023-10-22 18:41:35 -0300

    db

--- src/database.js
+++ src/database.js
@@ -1,8 +1,12 @@
 const Sequelize = require("sequelize");
+const { dbName, dbUser, dbPassword, dbHost } = require("./config/config");
 
-const sequelize = new Sequelize("NOMBREDATABASE", "NOMBREUSER", "PASSWORD", {
-  host: "localhost", // host de la base de datos
+const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
+  host: dbHost, // host de la base de datos
   dialect: "mysql",
 });
+
+
+
 
 module.exports = sequelize;
