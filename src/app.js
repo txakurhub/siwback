9a5942d1398027e191c8d99c17f6711fd89890c7
unknown <me.leandrop@gmail.com>
2023-10-23 18:20:38 -0300

    app & db cfg

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const routes = require('./routes');
 
 dotenv.config();
 
@@ -10,11 +11,4 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("SIWCARGO CHALLENGE BACKEND");
-});
-
-app.listen(port, ()=> {
-    console.log(`servidor corriendo en puerto ${port}`);
-})
+app.use(routes);
