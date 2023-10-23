7f3b8e6799c7e46aa511cc7622a18a8ad780e345
unknown <me.leandrop@gmail.com>
2023-10-23 18:24:24 -0300

    gitignore fix

--- index.js
+++
@@ -1,9 +0,0 @@
-const server = require("./src/app.js");
-const { conn } = require("./src/db.js");
-const { PORT } = process.env;
-
-conn.sync({ force: true }).then(() => {
-  server.listen(PORT, () => {
-    console.log("Servidor corriendo en el puerto 3001");
-  });
-});
--- src/routes/ticketRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', ticketController.getAllTickets);
-router.post('/', ticketController.createTicket);
-
-
-module.exports = router;
