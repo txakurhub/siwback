b4dc54269f27f77425bc4739926377bb4b61736e
unknown <me.leandrop@gmail.com>
2023-10-24 05:02:18 -0300

    auth controller & routes

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -48,3 +48,65 @@ exports.login = async (req, res) => {
     res.status(500).json({ error: "Error en el inicio de sesión" });
   }
 };
+
+// UPDATE
+
+exports.update = async (req, res) => {
+  const { username, password, newUsername, newPassword, newEmail } = req.body;
+
+  try {
+    const user = await User.findOne({ where: { username } });
+    if (!user) {
+      return res.status(401).json({ error: "Credenciales inválidas" });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ error: "Credenciales inválidas" });
+    }
+
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+    user.username = newUsername;
+    user.password = hashedPassword;
+    user.email = newEmail;
+
+    await user.save();
+
+    res.status(200).json({
+      message: "Usuario actualizado con éxito",
+      user: {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Error en la actualización del usuario" });
+  }
+};
+
+// GET USER INFO
+
+exports.getUserInfo = async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    const user = await User.findByPk(userId);
+    if (!user) {
+      return res.status(404).json({ error: "Cliente no encontrado" });
+    }
+
+    res.status(200).json({
+      user: {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Error al recuperar la información del cliente" });
+  }
+};
--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -4,6 +4,7 @@ const authController = require("../controllers/authController");
 
 router.get("/login", authController.login);
 router.post("/signup", authController.signup);
+router.put("/update", authController.update);
+router.get("/recover", authController.getUserInfo);
 
 module.exports = router;
- 
